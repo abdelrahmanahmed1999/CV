9e282b8c10e7a3d18139a196346d5df1215c16ef
abdelrahmanahmed1999 <abdelrahmanahmedtarek1999@gmail.com>
2020-08-19 15:20:02 +0200

    sample template

--- js/plugins.js
+++ js/plugins.js
@@ -221,7 +221,7 @@ window.onscroll=function(){
   let upperfootersection=document.querySelector(".upperfooter"),
     allimage=document.querySelectorAll(".upperfooter .image"),
     counter=0;
-  if(window.pageYOffset > 1300){
+  if(window.pageYOffset > 1500){
     let set=setInterval(function(){
       if(counter > 5){
         clearInterval(set);
